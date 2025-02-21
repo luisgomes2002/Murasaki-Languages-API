b10c140e2995a05a29ce0e31145faa123c4c0f81
Luis <luisgmgomes@gmail.com>
2025-02-21 15:38:10 -0300

    feat: first commit

---
+++ src/main/java/languages/murasaki/MurasakiLanguages/MurasakiLanguagesApplication.java
@@ -0,0 +1,13 @@
+package languages.murasaki.MurasakiLanguages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurasakiLanguagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurasakiLanguagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/languages/murasaki/MurasakiLanguages/MurasakiLanguagesApplicationTests.java
@@ -0,0 +1,13 @@
+package languages.murasaki.MurasakiLanguages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurasakiLanguagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
